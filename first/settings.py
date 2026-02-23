7220c55983547e1664418f5715552161035ff1d2
aabha3201 <70412921+aabha3201@users.noreply.github.com>
2026-02-23 01:27:48 -0600

    Update settings.py

--- first/settings.py
+++ first/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-fx-#vpwr_m8x5-!$*!*aqsort&*8gpht*e20gq0yu@vq2zkuww
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com']
 
 
 # Application definition
